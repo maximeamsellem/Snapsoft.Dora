1af2a20e1b93b9826ba9dade631711616ea89bb1
maxime amsellem <maxime.amsellem@gmail.com>
2023-12-17 09:51:20 +0100

    test: add component id test + get status not found check

--- src/Snapsoft.Dora.WebApi.Integration.Test/ComponentControllerTests.cs
+++ src/Snapsoft.Dora.WebApi.Integration.Test/ComponentControllerTests.cs
@@ -3,6 +3,7 @@ using Snapsoft.Dora.Domain.Contracts.Commands;
 using Snapsoft.Dora.WebApi.Dtos;
 using Snapsoft.Dora.WebApi.Integration.Test.Core;
 using System.Net;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Snapsoft.Dora.WebApi.Integration.Test
 {
@@ -21,10 +22,7 @@ namespace Snapsoft.Dora.WebApi.Integration.Test
         {
             // Arrange
             var createHttpRequest = BuildComponentHttpRequest();
-            var command = new CreateComponentCommand
-            {
-                Name = Guid.NewGuid().ToString(),
-            };
+            var command = BuildCreateComponentCommand();
 
             // Act
             var actual = await createHttpRequest.PostJsonAsync(command);
@@ -36,33 +34,81 @@ namespace Snapsoft.Dora.WebApi.Integration.Test
             Assert.Equal(body.Data?.Name, command.Name);
             Assert.True(body.Data?.Id > 0);
         }
-                
+
         [Fact]
-        public async Task GetComponent_Returns_It()
+        public async Task CreateComponent_Returns_Always_Increment_Id()
         {
             // Arrange
-            var createHttpRequest = BuildComponentHttpRequest();
-            var command = new CreateComponentCommand
-            {
-                Name = Guid.NewGuid().ToString(),
-            };
+            var command1 = BuildCreateComponentCommand();
+            var command2 = BuildCreateComponentCommand();
 
-            var createResponse = await createHttpRequest.PostJsonAsync(command);
-            Assert.Equal(createResponse?.StatusCode, (int)HttpStatusCode.Created);
-            var createdComponent = await createResponse!.GetJsonAsync<SuccessResponseDto<ComponentDto>>();
+            // Act            
+            var createdComponent1 = await CreateComponentWithChecks(command1);
+            var createdComponent2 = await CreateComponentWithChecks(command2);
+
+            // Assert
+            Assert.True(createdComponent1.Id < createdComponent2.Id);
+        }
+
+        [Fact]
+        public async Task GetComponent_Returns_Request_Component()
+        {
+            // Arrange
+            var command = BuildCreateComponentCommand();
+            var createdComponent = await CreateComponentWithChecks(command);
 
             // Act
-            var actual = await createHttpRequest
-                .AppendPathSegment(createdComponent.Data?.Id)
+            var actual = await BuildComponentHttpRequest()
+                .AppendPathSegment(createdComponent.Id)
                 .GetAsync();
 
             // Assert
             Assert.Equal(actual?.StatusCode, (int)HttpStatusCode.OK);
 
             var actualComponent = await actual!.GetJsonAsync<SuccessResponseDto<ComponentDto>>();
-            Assert.Equal(createdComponent.Data?.Name, command.Name);
-            Assert.NotNull(actualComponent?.Data?.Id);
-            Assert.Equal(createdComponent.Data?.Id, actualComponent?.Data?.Id);
+            Assert.Equal(createdComponent.Name, command.Name);            
+            Assert.Equal(createdComponent.Id, actualComponent?.Data?.Id);
+        }
+
+        [Fact]
+        public async Task GetComponent_Returns_Not_Found_When_Component_Does_Not_Exists()
+        {
+            // Act
+            var actual = await GetComponent(Random.Shared.NextInt64());
+
+            // Assert
+            Assert.Equal(actual?.StatusCode, (int)HttpStatusCode.NotFound);
+        }
+                
+        private static CreateComponentCommand BuildCreateComponentCommand()
+        {
+            return new CreateComponentCommand
+            {
+                Name = Guid.NewGuid().ToString(),
+            };
+        }
+
+        private async Task<ComponentDto> CreateComponentWithChecks(CreateComponentCommand createComponentCommand)
+        {
+            var createHttpRequest = BuildComponentHttpRequest();
+            var createResponse = await createHttpRequest.PostJsonAsync(createComponentCommand);
+
+            Assert.Equal(createResponse?.StatusCode, (int)HttpStatusCode.Created);
+            
+            var successDto = await createResponse!
+                .GetJsonAsync<SuccessResponseDto<ComponentDto>>();
+
+            Assert.NotNull(successDto?.Data);
+
+            return successDto.Data;
+
+        }
+
+        private async Task<IFlurlResponse> GetComponent(long componentId)
+        {
+            return await BuildComponentHttpRequest()
+                .AppendPathSegment(componentId)
+                .GetAsync();
         }
 
         private IFlurlRequest BuildComponentHttpRequest()
